46371564b817e1947c2a18893f99eea7bcba2d37
amir <zareamirhussein@gmail.com>
2021-07-09 18:33:04 +0430

    adding sign-up with firebase

--- src/pages/SignInPage/components/SignUpForm/SignUpForm.component.js
+++ src/pages/SignInPage/components/SignUpForm/SignUpForm.component.js
@@ -1,29 +1,39 @@
 import {InputForm, Button} from "../../../../components/index"
-import {signInWithGoogle} from "../../../../utils/firebase.utils"
+import {auth, createUserProfileDocument} from "../../../../utils/firebase.utils"
 import {useState, useEffect} from "react"
 
 function SignUpForm(){
 
-    const [state, setState] =  useState({ password:'', username:'',})
+    const [state, setState] =  useState({ password:'', email:'',})
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault()
-        setState({  password:'', username:'', })
+        const { password, email } = state
+        try {
+            console.log(password, email)
+            const {user} = await auth.createUserWithEmailAndPassword( email, password )            
+            console.log(typeof user)
+            await createUserProfileDocument(user)
+            setState({  password:'', email:'' })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     const handleChange = ({target}) => {
         const {name, value} = target
-        setState({ [name] : value })
+        console.log(name, value)
+        setState({...state, [name] : value })
     }
 
-    const { password, username} = state
+    const { password, email} = state
     return (
         <form onSubmit={handleSubmit} >
             <h3>Sign Up</h3>
             <h4 className="form-message">You don't have an account</h4>
-            <InputForm value={username} label="username" type="text" name="username" handleChange={handleChange}/>
+            <InputForm value={email} label="email" type="text" name="email" handleChange={handleChange}/>
             <InputForm value={password} label="password" type="password" name="password" handleChange={handleChange}/>
-            <Button className="signin-button" type="submit" onClick={signInWithGoogle}>sign up</Button>
+            <Button className="signin-button" type="submit">sign up</Button>
         </form>)
 }
 
