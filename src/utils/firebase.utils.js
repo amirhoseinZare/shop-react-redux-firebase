840b15485058a7576803abf54ef4d4bbfec7dbc1
amir <zareamirhussein@gmail.com>
2021-07-09 17:46:06 +0430

    adding create user

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
-
 const config = {
     apiKey: "AIzaSyCWep7fknAxMD0_UulsxLdL1ghHCsywYgU",
     authDomain: "shop-c3cdd.firebaseapp.com",
@@ -20,5 +19,30 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ propmt:'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth)
+        return null;
+    const userRef = firestore.doc(`user/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if(!snapShot.exists){
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return userRef
+}
 
 export default firebase
